bdb258bb3b9c98f577d6cf474871429207d7c4ed
TomekBekus02 <156850117+TomekBekus02@users.noreply.github.com>
2025-02-22 18:32:53 +0100

    added product styling display

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,15 +1,20 @@
 import '../../styles/Product.css'
+import '../../styles/utilStyles/utilStyles.css'
 
 export default function product({id, imgURL, title, price}){
     return (
-        <div key={id} className="product">
-            <h2>{title}</h2>
-            <div className='imgContainer'>
-                <img src={`${imgURL}`} alt={`${title}`} />
-                <p>{price}</p>
+        <div key={id} className="product-box">
+            <div className="product">
+                <div className='imgContainer'>
+                    <img src={`${imgURL}`} alt={`${title}`} />
+                    <p>{price}zł</p>
+                </div>
+                <h2>{title}</h2>
+                <div className="button-container">
+                    <button className='details-button btn'>Details</button>
+                    <button className='add-cart-button btn'>Add to Cart</button>
+                </div>
             </div>
-            <button>Details</button>
-            <button>Add to Cart</button>
         </div>
     )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Query, useQuery } from "@tanstack/react-query"
 import axios from "axios"
 
 import Product from '../components/Product'
+import "../../styles/Main.css"
 
 const fetchProducts = async () => {
     const { data } = await axios.get('http://localhost:3000/products')
@@ -14,24 +15,26 @@ export default function Home(){
     const { data: products, isLoading, isError } = useQuery({queryKey: ["Products"], queryFn: fetchProducts})
     return (
         <div>
-            <h1>Home Page</h1>
-            {
-                isLoading 
-                    ? <p>Loading...</p> 
-                    : products.length > 0
-                        ?products.map(product => {
-                            return (
-                                <Product 
-                                    id={product._id}
-                                    title={product.title}
-                                    imgURL={product.imgUrl}
-                                    price={product.price}
-                                />
-                            )
-                        })
-                        : <p>No Products</p>
+            <input type="text" placeholder="Search" id="filtredProduct" name="filtredProduct" />
+            <div className="products">
+                {
+                    isLoading 
+                        ? <p>Loading...</p> 
+                        : products.length > 0
+                            ? products.map(product => {
+                                return (
+                                    <Product 
+                                        id={product._id}
+                                        title={product.title}
+                                        imgURL={product.imgUrl}
+                                        price={product.price}
+                                    />
+                                )
+                            })
+                            : <p>No Products</p>
 
-            }
+                }
+            </div>
         </div>
     )
 }
